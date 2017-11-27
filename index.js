c8e8a9fe86f0bc312520234058cfd678f4b08f66
Võ Lê Thanh Phú <volethanhphu174@gmail.com>
2017-11-27 09:59:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
